b9b1869b83a92568fc4397e8b0e4a38b706c32f7
= <=>
2021-03-05 10:44:53 -0300

    first commit

---
+++ app/src/main/java/com/example/simplegradecalculator/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.simplegradecalculator
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnCalculate = calculateButton
+        val result = result
+
+        btnCalculate.setOnClickListener {
+
+            val grade1 = Integer.parseInt(grade1.text.toString())
+            val grade2 = Integer.parseInt(grade2.text.toString())
+            val absences = Integer.parseInt(absences.text.toString())
+
+            val mean = (grade1 + grade2) / 2
+
+            if(mean >= 8 && absences <= 4) {
+                result.text = """
+                                    Student passed UwU
+                                    Final grade: $mean
+                                    Absenses: $absences
+                                """.trimIndent()
+                result.setTextColor(Color.GREEN)
+            }
+
+            else {
+                result.text = """
+                                    Student failed v_v
+                                    Final grade: $mean
+                                    Absenses: $absences
+                                """.trimIndent()
+                result.setTextColor(Color.RED)
+            }
+        }
+    }
+}
